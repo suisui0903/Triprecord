962316e0e791e06fd0beefbd5004f740f2344ca1
suisui0903 <suisuisui0903@gmail.com>
2020-10-31 15:03:42 +0900

    シークレットキーの設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6b5420fd1800322c621c72f9a9423917eabab19e1e3d97babc1b6ae0c92d277963ae48f88e9aab5c380e932bf48ed3855916b2a425d8cf7f2d73a7dabd4ab177'
